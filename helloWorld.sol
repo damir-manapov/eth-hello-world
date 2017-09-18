b0d35272c3bab66ba06fb6971c9956c870ade4e2
Damir <manapov.d@natalie-tours.ru>
2017-09-18 09:02:47 +0300

    HelloWorld contract

---
+++ helloWorld.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.13;
+contract HelloWorld {
+    uint256 counter = 0;
+
+    function increase() {
+        counter++;
+    }
+
+    function decrease() {
+        counter--;
+    }
+
+    function getCounter() constant returns(uint256) {
+        return counter;
+    }
+}
